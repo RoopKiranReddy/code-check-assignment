3efe100bad9e4088fc21c4f3ca57def14b4ba298
ROOP KIRAN REDDY <113887293+RoopKiranReddy@users.noreply.github.com>
2023-01-04 19:50:38 +0530

    Add files via upload

---
+++ coupon.js
@@ -0,0 +1,11 @@
+
+function loadCoupon(){
+    document.getElementsByClassName('coupon')[0].style.display="block";
+    document.getElementsByTagName('body').style.opacity="0.2";
+}
+
+function closeCoupon(){
+    document.getElementsByClassName('coupon')[0].style.display="none";
+    document.getElementsByTagName('body').style.opacity="1";
+}
+
